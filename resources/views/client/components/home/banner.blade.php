37fe32576a82df26671d2999fe1177a362649011
Dac-Phuong <nguyendacphuong14@gmail.com>
2025-02-17 23:52:32 +0700

    update

--- resources/views/client/components/home/banner.blade.php
+++ resources/views/client/components/home/banner.blade.php
@@ -2,51 +2,55 @@
     <div class="slider-container w-100 h-100">
         <div class="swiper swiper-main w-100 h-100">
             <div class="swiper-wrapper">
-                @foreach ($banner as $item)
-                    <div class="swiper-slide d-flex flex-column align-items-center justify-content-center w-100 h-100 text-center">
-                        <img src="{{ $item['thumbnail'] }}" alt="{{ $item['thumbnail'] }}" height="100%" class="img-fluid">
-                        <div class="slide-content">
-                            <h2 class="text-white fs-2 text-shadow">{{ $item['title'] }}</h2>
-                            <p class="text-white mb-3 fs-5 m-auto text-description">{{ $item['description'] }}</p>
-                            <button class="btn btn-primary next-slide rounded-pill">Xem thêm</button>
+                @if (isset($banner) && count($banner) > 0)
+                    @foreach ($banner as $item)
+                        <div
+                            class="swiper-slide d-flex flex-column align-items-center justify-content-center w-100 h-100 text-center">
+                            <img src="{{ $item['thumbnail'] }}" alt="{{ $item['thumbnail'] }}" height="100%"
+                                class="img-fluid">
+                            <div class="slide-content">
+                                <h2 class="text-white fs-2 text-shadow">{{ $item['title'] }}</h2>
+                                <p class="text-white mb-3 fs-5 m-auto text-description">{{ $item['description'] }}</p>
+                                <button class="btn btn-primary next-slide rounded-pill">Xem thêm</button>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                @endif
             </div>
         </div>
     </div>
 </section>
 
- @push('scripts')
-     <script>
-         $(document).ready(function() {
-             const mainSwiper = new Swiper('.swiper-main', {
-                 spaceBetween: 10,
-                 navigation: {
-                     nextEl: '.swiper-button-next',
-                     prevEl: '.swiper-button-prev',
-                 },
-                 effect: 'fade',
-                 fadeEffect: {
-                     crossFade: true
-                 },
-                 autoplay: {
-                     delay: 5000,
-                     disableOnInteraction: false,
-                 }
-             });
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            const mainSwiper = new Swiper('.swiper-main', {
+                spaceBetween: 10,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                effect: 'fade',
+                fadeEffect: {
+                    crossFade: true
+                },
+                autoplay: {
+                    delay: 5000,
+                    disableOnInteraction: false,
+                }
+            });
 
-             mainSwiper.on('slideChange', function() {
-                 const activeSlide = $(mainSwiper.slides[mainSwiper.activeIndex]);
-                 const imageUrl = activeSlide.find('img').attr('src');
-             });
-             $(document).on('click', '.next-slide', function() {
-                 if (mainSwiper.activeIndex === mainSwiper.slides.length - 1) {
-                     mainSwiper.slideTo(0);
-                 } else {
-                     mainSwiper.slideNext();
-                 }
-             });
-         });
-     </script>
- @endpush
+            mainSwiper.on('slideChange', function() {
+                const activeSlide = $(mainSwiper.slides[mainSwiper.activeIndex]);
+                const imageUrl = activeSlide.find('img').attr('src');
+            });
+            $(document).on('click', '.next-slide', function() {
+                if (mainSwiper.activeIndex === mainSwiper.slides.length - 1) {
+                    mainSwiper.slideTo(0);
+                } else {
+                    mainSwiper.slideNext();
+                }
+            });
+        });
+    </script>
+@endpush
